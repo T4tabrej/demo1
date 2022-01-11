72683f4f53b2a2dedc1caaa7526deea24e2db651
Tabrej <tabrejhussain7@gmail.com>
2022-01-11 17:19:26 +0530

    kj

--- src/app.js
+++ src/app.js
@@ -65,7 +65,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
